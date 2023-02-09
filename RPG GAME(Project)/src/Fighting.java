8b9b0fa376621fe3b765c41508ae8f68c8158088
Ryukiyian <122075153+Ryukiyian@users.noreply.github.com>
2023-02-09 06:39:00 -0600

    Add files via upload

---
+++ RPG GAME(Project)/src/Fighting.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Fighting {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        int level = 1;
+        int experience = 0;
+        int health = 100;
+        System.out.print("Please add your attribute points to your character");
+
+        while(health > 0 && health <=100)
+        {
+    int StreetThugAttk = 1;
+    int StreetThugDef = 1;
+    int StreetThugHP = 50;
+        System.out.print("A lvl 1 street thug suddenly appears in front of you. He seems to be trying to rob you.");
+        System.out.print("Lvl 1 Street Thug" + "\n" + "Health " + StreetThugHP);
+int health = 0;
+
+
+        }
+    }
+}

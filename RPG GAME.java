e9364585e1a8f6a5f14da5ba2dfc2dfd07743971
Ryukiyian <122075153+Ryukiyian@users.noreply.github.com>
2023-02-08 18:12:26 -0600

    Add files via upload

---
+++ RPG GAME.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Triangle
+{
+    public static void main (String[]args) {
+        Scanner scan = new Scanner(System.in);
+        int health = 100;
+        while(health == 100)
+        {
+
+        }
+    }
+}
